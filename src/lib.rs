b8848af5a109953eeb6e0885d54b8dbc6c8b5d9a
Ricardo Mendes <rokusu@gmail.com>
2017-06-22 16:25:50 +0100

    Ran `cargo fmt` on the codebase

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,15 @@ pub fn tico(tico: &str) -> String {
     let mut shortened = String::from("");
     let mut skip_char = false;
     let mut count = 0;
-    let sections = tico.chars().filter(|&x| x == '/' ).count();
+    let sections = tico.chars().filter(|&x| x == '/').count();
 
     for c in tico.chars() {
         match c {
-            '~' => if !skip_char { shortened.push(c) },
+            '~' => {
+                if !skip_char {
+                    shortened.push(c)
+                }
+            }
             '.' => {
                 skip_char = false;
                 shortened.push(c);
@@ -19,8 +23,7 @@ pub fn tico(tico: &str) -> String {
             _ => {
                 if skip_char && count < sections {
                     continue;
-                }
-                else {
+                } else {
                     skip_char = true;
                     shortened.push(c);
                 }
@@ -35,7 +38,10 @@ pub fn tico(tico: &str) -> String {
 fn it_works() {
     assert_eq!(tico("~"), "~");
     assert_eq!(tico("/"), "/");
-    assert_eq!(tico("/home/hugopeixoto/work/personal/tico"), "/h/h/w/p/tico");
+    assert_eq!(
+        tico("/home/hugopeixoto/work/personal/tico"),
+        "/h/h/w/p/tico"
+    );
     assert_eq!(tico("~/work/personal/tico"), "~/w/p/tico");
     assert_eq!(tico("~/work/personal/tico/"), "~/w/p/t/");
     assert_eq!(tico("~/work/ééé/tico"), "~/w/é/tico");
--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,7 @@ use std::io::Write;
 fn main() {
     let path = std::env::args().nth(1).unwrap_or(String::from(""));
 
-    std::io::stdout().write(tico::tico(&path).as_bytes()).unwrap();
+    std::io::stdout()
+        .write(tico::tico(&path).as_bytes())
+        .unwrap();
 }
