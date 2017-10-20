d8b3e429bf0a32d8bb3d96f1aaeb84ffaec3aca9
Ricardo Mendes <rokusu@gmail.com>
2017-10-20 12:24:08 +0100

    set up documentation

---
+++ search-index.js
@@ -0,0 +1,3 @@
+var searchIndex = {};
+searchIndex["tico"] = {"doc":"","items":[[5,"tico","tico","Shorten a path.",null,{"inputs":[{"name":"str"}],"output":{"name":"string"}}]],"paths":[]};
+initSearch(searchIndex);
